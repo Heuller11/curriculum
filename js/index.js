8090d0d274234f4235476dcfd958db1237f9278b
Heuller <heuller555@gmail.com>
2022-09-10 12:11:43 -0300

    timeline responsive, add text and figures

--- js/index.js
+++ js/index.js
@@ -9,3 +9,14 @@ $('nav .bi-x-circle').click(function(){
     $('.overlay').css('display', 'none');
     $('.bi-x-circle').css('display', 'none')
 })
+
+
+$('.flag svg').click(function(){
+    $(this).parents('div.wrap-accessories').find('.box').css('display', 'block');
+    $('.overlay').css('display', 'block');
+})
+
+$('.box svg').click(function(){
+    $(this).parent().css('display', 'none');
+    $('.overlay').css('display', 'none');
+})
