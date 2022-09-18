ab51e1ebb8c7c6b915b3de19e7ca32793c05792e
Heuller <heuller555@gmail.com>
2022-09-18 17:48:37 -0300

    finished curriculum, add right directory

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $('nav .bi-list').click(function(){
     $('.menu').css('display', 'block');
     $('.overlay').css('display', 'block');
-    $('.bi-x-circle').css('display', 'block')
+    $('.closeMenu').css('display', 'block')
 })
 
-$('nav .bi-x-circle').click(function(){
+$('nav .closeMenu, .menu li').click(function(){
     $('.menu').css('display', 'none');
     $('.overlay').css('display', 'none');
-    $('.bi-x-circle').css('display', 'none')
+    $('.closeMenu').css('display', 'none')
 })
 
 
