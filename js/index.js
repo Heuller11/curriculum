08940a5f27b6268c889cd80537836a2bd031a199
Heuller <heuller555@gmail.com>
2022-09-18 16:05:03 -0300

    missing add texts and change color image

--- js/index.js
+++ js/index.js
@@ -12,14 +12,27 @@ $('nav .bi-x-circle').click(function(){
 
 
 $('.info-icon').click(function(){
-    var index = $('.timeline-content .description').index($(this).parents('.description'));
+
+    var description = $(this).parents('.description');
+
+    var index = $('.timeline-content .description').index(description);
     var calculateTop = index < 2 ? (105 + index * 320).toString() + 'px' : (105 + 80 + index * 320).toString() + 'px';
+
+    
+    var activity = $(description).find('.activity');
+    var title = $(activity).find('.activity-title').html();
+    var text = $(activity).find('p').html();
+
+    var box_elements = $('.box-responsive .box-elements');
+    $(box_elements).find('h5').text(title);
+    $(box_elements).find('p').text(text);
+
     $('.box-responsive').css({'display': 'block',
                                 'top': calculateTop});
     $('.overlay').css('display', 'block');
 })
 
-$('.box svg').click(function(){
-    $(this).parent().css('display', 'none');
+$('.box-elements svg').click(function(){
+    $(this).parents('.box-responsive').css('display', 'none');
     $('.overlay').css('display', 'none');
 })
